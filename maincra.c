df6965c1d3d47e95c92ccffc4c2c06a8bd2b168e
logralahad <61020509+logralahad@users.noreply.github.com>
2020-03-04 21:42:45 -0600

    Add files via upload

---
+++ maincra.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#define SIZE 1000
+
+int main(){
+
+    long long int n, k;
+    long long int suma1 = 0, suma2 = 0;
+    long long int diamantes[SIZE];
+
+    //cin >> n;
+    scanf("%lld", &n);
+
+    for(long long int i = 0; i < n; i++){
+        //cin >> diamantes[i];
+        scanf("%lld", &diamantes[i]);
+    }
+    //cin >> k;
+    scanf("%lld", &k);
+
+    for(long long int i = 0; i < n; i++){
+        if((n - i) < k) continue;
+        for(long long int  j = 0; j < k; j++){
+            suma1 += diamantes[i + j];
+        }
+        if(suma1 >= suma2){
+            suma2 = suma1;
+            suma1 = 0;
+        }
+        else{
+            suma1 = 0;
+        }
+    }
+
+    printf("%lld", suma2);
+    //cout << suma2;
+
+return 0;
+}
