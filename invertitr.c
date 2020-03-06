b0600462560b51d5635763d011e25addc9785952
logralahad <61020509+logralahad@users.noreply.github.com>
2020-03-06 11:57:23 -0600

    Update invertitr.c

--- invertitr.c
+++ invertitr.c
@@ -1,23 +1,35 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
-#define SIZE 100
 
 
-void voltea_cadena(char arr[], int k){
-    for(int i = k - 1; i >= 0; i--){
-        printf("%c", arr[i]);
-    }
+char* voltea_cadena(char arr[]) {
+  
+  int largo_cadena = strlen(arr) - 1;
+  char temp;
+
+  for (int i = 0; i <= (largo_cadena / 2); i++) {
+    temp = arr[i];
+    arr[i] = arr[largo_cadena - i];
+    arr[largo_cadena - i] = temp;
+  }
+
+  return arr;
 }
 
-int main(){
 
-    char cadena[SIZE];
+int main()
+{
 
-    printf("Escriba la cadena a revertir: \n");
-    gets(cadena);
+  //Inicialización de la cadena 
+  char str[100] = "Extrano a mi ex  = xe im a onartxE", * new_str;
+  //printf("Ingrese un texto: %s\n", str);
 
-    int j = strlen(cadena);
-    voltea_cadena(cadena, j);
+  // Voltear la cadena 
+  new_str = voltea_cadena(str);
+  //new_str = _strrev(str);
 
-return 0;
+  // Imprimir el resultado 
+  printf("La cadena volteada: %s\n", new_str);
+
+  return 0;
 }
