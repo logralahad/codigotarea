a25e22fdf0c234b9bad2ac51cb891837da086d1e
logralahad <61020509+logralahad@users.noreply.github.com>
2020-05-07 10:32:25 -0500

    Update personabinaria.cpp

--- personabinaria.cpp
+++ personabinaria.cpp
@@ -15,26 +15,59 @@ typedef struct persona
 
 int main(int argc, char const *argv[]){
 
-    FILE *miaarchivo;
-    Persona arr[10];
+    Persona array_de_personas[10];
+    char c;
 
+    FILE *miaarchivo;
     miaarchivo = fopen("miarchivo.bin", "rb");
 
     if(miaarchivo == NULL){
         printf("Error al abrir el archivo.\n");
-        return(1);
+        for(int i = 0; i < 2; i++){
+            printf("\n\n\n");
+            array_de_personas[i].id_persona = i+1;
+
+            printf("Introduzca su nombre completo:\n");
+            fgets(array_de_personas[i].nombre, 99,stdin);
+
+            printf("Introduzca el sexo de la persona [H/M]:\n");
+            array_de_personas[i].sexo = getc(stdin);
+            while ((c = getc(stdin)) != '\n' && c != EOF); //Esta linea hace flush a la entrada
+
+            printf("Introduzca la direccion:\n");
+            fgets(array_de_personas[i].direccion, 99,stdin);
+
+            printf("Introduzca la religion:\n");
+            fgets(array_de_personas[i].religion, 49,stdin);
+
+            printf("Introduzca la escolaridad:\n");
+            fgets(array_de_personas[i].escolaridad, 19,stdin);
+
+            printf("Introduzca la edad:\n");
+            scanf("%hu", &array_de_personas[i].edad);
+            while ((c = getc(stdin)) != '\n' && c != EOF); //Esta linea hace flush a la entrada
+
+            miaarchivo = fopen("miarchivo.bin", "wb");
+            if(miaarchivo == NULL){
+                printf("Error al abrir archivo.\n");
+                return(1);
+            }
+        }
+        fwrite(array_de_personas, sizeof(Persona), 2, miaarchivo);
     }
 
-    printf("Estas son las personas registradas:\n");
-    fread(arr, sizeof(Persona),10,miaarchivo); //lee cada linea y las almacena en el arr de personas
-    for(int i = 0; i < 10; i++){//inicia la secuencia de impresion de cada dato de la persona
-        printf("%d\n",arr[i].id_persona);
-        printf("%s\n",arr[i].nombre);
-        printf("%c\n",arr[i].sexo);
-        printf("%s\n",arr[i].direccion);
-        printf("%s\n",arr[i].religion);
-        printf("%s\n",arr[i].escolaridad);
-        printf("%d\n",arr[i].edad);
+    else{
+        printf("Estas son las personas registradas:\n");
+        fread(array_de_personas, sizeof(Persona),2,miaarchivo); //lee cada linea y las almacena en el arr de personas
+        for(int i = 0; i < 2; i++){//inicia la secuencia de impresion de cada dato de la persona
+            printf("%d\n",array_de_personas[i].id_persona);
+            printf("%s\n",array_de_personas[i].nombre);
+            printf("%c\n",array_de_personas[i].sexo);
+            printf("%s\n",array_de_personas[i].direccion);
+            printf("%s\n",array_de_personas[i].religion);
+            printf("%s\n",array_de_personas[i].escolaridad);
+            printf("%d\n",array_de_personas[i].edad);
+        }
+        fclose(miaarchivo);
     }
-    fclose(miaarchivo);
 }
